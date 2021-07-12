51f1b4f4fcc3223474e1bd185f20dd55c2609354
Shanto <kamrul0628@gmail.com>
2021-07-12 21:22:25 +0600

    DP problems codes

---
+++ 3_Count_Occurrences_Of_Anagrams_Sliding_Window.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+
+int countOccurrencesOfAnagrame(string s,string ptr){
+
+    map<char,int> mp;
+    for (int i = 0; i < ptr.size(); ++i)
+    {
+        mp[ptr[i]]++;
+    }
+
+    int i=0,j=0;
+    int ans = 0;
+    int k = ptr.size();
+    int count = mp.size();
+    while(j < s.size()){    
+
+        mp[s[j]]--;
+        if(mp[s[j]] == 0){
+            count--;
+        }
+        if(j-i+1 < k){
+            j++;
+        }
+        else if(j-i+1 == k){
+            
+            mp[s[i]]++;
+            if(count == 0){
+                ans++;
+            }
+            if(mp[s[i]] >= 1){
+                count++;
+            }
+            i++;
+            j++;
+        }
+    }
+
+    return ans;
+
+}
+
+
+
+int main(){
+    
+    // string s = "aabaabaa";
+    // string ptr = "aaba";
+
+    string s = "forxxorfxdofr";
+    string ptr = "for";
+
+
+  
+    cout<<countOccurrencesOfAnagrame(s, ptr);
+
+}
--- test.cpp
+++ test.cpp
@@ -3,8 +3,51 @@ using namespace std;
  
 
 
-int main()
-{
+
+vector<int> firstNegativeInEveryWindow(int arr[],int k,int n){
+
+    vector<int> v;
+    list<int> l;
+
+    int i = 0;
+    int j = 0;
+
+    while(j < n){
+        if(arr[j] < 0){
+            l.push_back(arr[j]);
+        }
+        if(j-i+1 < k){
+            j++;
+        }
+        else if(j-i+1 == k){
+            if(l.size() == 0){
+                v.push_back(0);
+            }
+            else{
+                v.push_back(l.front());
+                if(arr[i] == l.front()){
+                    l.pop_front();
+                }
+            }
+            i++;
+            j++;
+        }
+    }
+
+    return v;
+}
+
+
+int main(){
     
-    return 0;
+    int arr[] = {12, -1, -7, 8, -15, 30, 16, 28};
+    int n = 8;
+    int k = 3;
+
+    vector<int> v = firstNegativeInEveryWindow(arr,k,n);
+
+    for(auto at : v){
+        cout<<at<<" ";
+    }
+
 }
